206c25ff11f5906e69be3adf987f3c07d67ca037
nt0808 <kisala10@hotmail.de>
2023-03-27 19:52:29 +0200

    sechster Commit

---
+++ uebung-02.js
@@ -0,0 +1,12 @@
+var course = { 
+    title: 'mmsys1', 
+    room: 10, 
+    description: 'Schwerpunkt: Grundlagen von JavaScript', 
+    credits: 4, 
+    num_students: 5 
+ }; 
+
+ //2.1
+ //a
+delete course.description;
+console.log(course);
