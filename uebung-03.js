a0e348afe29176eebc616249a217b019b3105e26
nt0808 <kisala10@hotmail.de>
2023-03-29 09:52:58 +0200

    sechster commit

--- uebung-03.js
+++ uebung-03.js
@@ -1,16 +1,16 @@
+//3.1
+//a
 var word1 = 'informationsmanagement'; 
 var word2 = 'multimediasysteme'; 
 var text;
 
-//3.1
-//a
 if (word1.length > word2.length) {
     text = 'Das erste Wort ist länger.';
 } else if (word1.length = word2.length) {
     text = 'Das zweite Wort ist länger.';
 } else {
     text = 'Die Wörter sind gleich lang.';
-}
+};
 
 console.log(text);
 
@@ -21,7 +21,7 @@ var list = [];
 while (i<5) {
     list[i] = i*10;
     i++;
-}
+};
 
 console.log(list);
 
@@ -33,8 +33,27 @@ for (; i < 10; i++) {
     if (numList.includes(i)) {
         console.log(i);
     }
-}
+};
 
 //3.2
 //a
+function numberIsOdd (num) {
+    return (num % 2) != 0;
+};
+
 //b
+function cantineActionDay (day) {
+    var text = '';
+    if (day.includes('')) {
+        if (day.toLowerCase() === 'monday') {
+            text = 'taco monday';
+        } else if (day.toLowerCase() === 'wednesday') {
+            text = 'pizza wednesday';
+        } else if (day.toLowerCase() === 'friday') {
+            text = 'pasta friday';
+        } else {
+            text = 'no action'
+        }
+    }
+    return text;
+};
