b52710777fca57b57b2bc73055ef1a2ae3cb7449
nt0808 <kisala10@hotmail.de>
2023-03-29 16:17:50 +0200

    sechster commit

--- uebung-04.js
+++ uebung-04.js
@@ -1,3 +1,20 @@
 //4.1
 //a
-document.getElementByID('id1').innerHTML = 'Titel H1';
+document.write('<h1>Titel H1</h1>');
+
+//b
+var h5 = document.getElementById('subHeadline').style.fontSize = '18px';
+
+//c
+var imageSource = document.getElementById('img1').src;
+console.log(imageSource);
+
+//d
+var newText = document.getElementsByTagName('p')[1].innerHTML = 'Hallo. Ich bin ein kleiner Blindtext. Und zwar schon so lange ich denken kann. Es war nicht leicht zu verstehen, was es bedeutet, ein blinder Text zu sein: Man ergibt keinen Sinn.';
+
+//e
+var num = document.getElementsByClassName('list')[0].children.length;
+console.log(num);
+
+//4.2
+//a
