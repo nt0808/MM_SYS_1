fd0282f2f14f3f31a05cff1416e603bd1e0881fe
nt0808 <kisala10@hotmail.de>
2023-04-17 13:37:07 +0200

    second commit

--- uebung-01.js
+++ uebung-01.js
@@ -1,5 +1,5 @@
-var students = ['Anna', 'Matt', 'Giovanni', 'Julie', 'Vlad', 'Yoona'];
-var newStudents = ['Leonard', 'Mai', 'Rana', 'Melis'];
+var students = ["Anna", "Matt", "Giovanni", "Julie", "Vlad", "Yoona"];
+var newStudents = ["Leonard", "Mai", "Rana", "Melis"];
 
 //1.1
 //a
@@ -7,11 +7,11 @@ students.shift();
 console.log(students);
 
 //b)
-students[3] = 'Alex';
+students[3] = "Alex";
 console.log(students);
 
 //c)
-students.splice(1,3);
+students.splice(1, 3);
 console.log(students);
 
 //d)
@@ -21,38 +21,36 @@ console.log(students.toString());
 students = students.concat(newStudents);
 console.log(students);
 
-
 var teams = [
- ['Ruby', 'Angel'],
- ['Mia', 'Yui', 'Merve', 'Agathe', ['Min']],
- ['Nattapong', 'Giulia', 'Alva']
+  ["Ruby", "Angel"],
+  ["Mia", "Yui", "Merve", "Agathe", ["Min"]],
+  ["Nattapong", "Giulia", "Alva"],
 ];
 
 //1.2
 //a)
 function findName(arr, str) {
-	return arr.flat(Infinity).find(element => element === str);
-};
+  return arr.flat(Infinity).find((element) => element === str);
+}
 
-console.log(findName(teams, 'Min'));
+console.log(findName(teams, "Min"));
 
 //b)
 function findName2(arr, str) {
-    const ind = arr.findIndex(element => element.includes(str));
-    return `${ind}: ${arr[ind]}`;
-};
+  const ind = arr.findIndex((element) => element.includes(str));
+  return `${ind}: ${arr[ind]}`;
+}
 
-console.log(findName2(teams,'Giulia'));
-
+console.log(findName2(teams, "Giulia"));
 
 //c)
 function newteam(array) {
-    array.forEach((array2) => {
-        array2.reverse();
-    });
-    
-    array = array.flat(Infinity);
-    return array;
-};
+  array.forEach((array2) => {
+    array2.reverse();
+  });
+
+  array = array.flat(Infinity);
+  return array;
+}
 
 console.log(newteam(teams));
--- uebung-02.js
+++ uebung-02.js
@@ -1,22 +1,22 @@
-var course = { 
-    title: 'mmsys1', 
-    room: 10, 
-    description: 'Schwerpunkt: Grundlagen von JavaScript', 
-    credits: 4, 
-    num_students: 5 
- }; 
+var course = {
+  title: "mmsys1",
+  room: 10,
+  description: "Schwerpunkt: Grundlagen von JavaScript",
+  credits: 4,
+  num_students: 5,
+};
 
- //2.1
- //a
+//2.1
+//a
 delete course.description;
 console.log(course);
 
 //b
-course['room'] = 20;
+course["room"] = 20;
 console.log(course);
 
 //c
-course['lecturer'] = 'Rose';
+course["lecturer"] = "Rose";
 console.log(course);
 
 //d
@@ -24,53 +24,52 @@ arrcourse = Object.entries(course);
 console.log(arrcourse);
 
 //e
-var moreCourseInformations = { 
-    building: 'E12',  
-    students: ['Anna', 'Tim', 'Lukas', 'Julia', 'Lena'] 
+var moreCourseInformations = {
+  building: "E12",
+  students: ["Anna", "Tim", "Lukas", "Julia", "Lena"],
 };
 
-course = {...course, ...moreCourseInformations};
+course = { ...course, ...moreCourseInformations };
 console.log(course);
-
 
 //2.2
 //a
-var library = {  
-    books: {total: 18000, subjects: 193400, digital: 12200}, 
-    cds: {total: 1320, subjects: 2339}, 
-    videos: {total: 9008, subjects: 8273, digital: 1299} 
-}; 
+var library = {
+  books: { total: 18000, subjects: 193400, digital: 12200 },
+  cds: { total: 1320, subjects: 2339 },
+  videos: { total: 9008, subjects: 8273, digital: 1299 },
+};
 
 console.log(library.books.digital);
 
 //b
-var books = [ 
-    { 
-      title: 'JavaScript for impatient programmers',  
-      isbn: 1091210098,  
-      pages: 526,  
-      language: 'en' 
-    }, 
-    { 
-      title: 'Client Side Scripting: Using JavaScript',  
-      isbn: 9333223878,  
-      pages: 164,  
-      language: 'en' 
-    } 
- ]; 
+var books = [
+  {
+    title: "JavaScript for impatient programmers",
+    isbn: 1091210098,
+    pages: 526,
+    language: "en",
+  },
+  {
+    title: "Client Side Scripting: Using JavaScript",
+    isbn: 9333223878,
+    pages: 164,
+    language: "en",
+  },
+];
 
 console.log(books[1].pages);
 
 //c
-var student = { 
-    id: 1391,  
-    firstName: 'Jonas',  
-    lastName: 'Schulz',  
-    major: 'bim',
-    fullName: function() {
-      return this.firstName + " " + this.lastName;
-    }
- };  
+var student = {
+  id: 1391,
+  firstName: "Jonas",
+  lastName: "Schulz",
+  major: "bim",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
 
 var name = student.fullName();
 console.log(name);
