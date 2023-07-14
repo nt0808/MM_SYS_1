c7bb7a6e13acc72b12d343c9612ed1ce11cb0743
nt0808 <kisala10@hotmail.de>
2023-07-15 00:45:40 +0200

    5th commit

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var images = [
-    'café.png',
-    'caféclosedeyes.png',
-    'café.png',
-    'cafésmiling.png',
-    'café.png',
-    'café.png'
+  "café.png",
+  "caféclosedeyes.png",
+  "café.png",
+  "cafésmiling.png",
+  "café.png",
+  "café.png",
 ];
 
-var backgroundContainer = document.querySelector('.background-container');
+var backgroundContainer = document.querySelector(".background-container");
 
 // Function to change the background image with delay and pause
 function changeBackgroundImage() {
-    var randomIndex = Math.floor(Math.random() * images.length);
-    var randomImage = images[randomIndex];
-    backgroundContainer.style.backgroundImage = 'url(' + randomImage + ')';
+  var randomIndex = Math.floor(Math.random() * images.length);
+  var randomImage = images[randomIndex];
+  backgroundContainer.style.backgroundImage = "url(" + randomImage + ")";
 }
 
 // Call the function initially to display the first background image immediately
@@ -21,7 +21,6 @@ changeBackgroundImage();
 
 // Set interval to change the background image every 5 seconds (5000 milliseconds)
 setInterval(changeBackgroundImage, 2000);
-
 
 function openMenuPopup(popupId) {
   var popup = document.getElementById(popupId);
