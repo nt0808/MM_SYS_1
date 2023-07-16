cfdbddc9654b0be8ce4f41cd80525276c85dbf25
nt0808 <kisala10@hotmail.de>
2023-07-16 16:37:12 +0200

    10th commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Array von den Bildern, die für die Funktion changeBackgroundImage angewendet werden
-var images = [
+var images2 = [
   "img/café.png",
   "img/caféclosedeyes.png",
   "img/café.png",
@@ -8,17 +8,15 @@ var images = [
   "img/café.png",
 ];
 
-var backgroundContainer = document.querySelector(".background-container");
-
 // Funktion, um das Hintergrundbild zufällig mit Delay und Pause zu ändern
-function changeBackgroundImage() {
+function changeBackgroundImage2() {
   var randomIndex = Math.floor(Math.random() * images.length);
   var randomImage = images[randomIndex];
   backgroundContainer.style.backgroundImage = "url(" + randomImage + ")";
 }
 
 // Funktion aufgerufen, damit das erste Hintergrundbild sofort aufgerufen wird
-changeBackgroundImage();
+changeBackgroundImage2();
 
 // Intervall, in dem sich das Hintergrundbild ändert (2000 ms)
 setInterval(changeBackgroundImage, 2000);
@@ -34,3 +32,57 @@ function closeMenuPopup(popupId) {
   var popup = document.getElementById(popupId);
   popup.style.display = "none";
 }
+
+// Function to load and process JSON data
+function loadCoffeeMenu() {
+  // Fetch the JSON file
+  fetch("coffee-menu.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Get the coffee menu container
+      const coffeeMenuContainer = document.getElementById("coffee");
+
+      // Loop through the menu items and create the HTML elements dynamically
+      data.menuItems.forEach((menuItem) => {
+        const menuItemElement = document.createElement("div");
+        menuItemElement.classList.add("menu-item");
+
+        const menuItemImage = document.createElement("div");
+        menuItemImage.classList.add("menu-item-image");
+        const img = document.createElement("img");
+        img.src = menuItem.image;
+        img.alt = menuItem.title;
+        menuItemImage.appendChild(img);
+        menuItemElement.appendChild(menuItemImage);
+
+        const menuItemDetails = document.createElement("div");
+        menuItemDetails.classList.add("menu-item-details");
+        const menuContainer = document.createElement("div");
+        menuContainer.classList.add("menu-container");
+        const titleElement = document.createElement("h2");
+        titleElement.classList.add("menu-item-title");
+        titleElement.textContent = menuItem.title;
+        const priceElement = document.createElement("span");
+        priceElement.classList.add("menu-item-price");
+        priceElement.textContent = menuItem.price;
+        menuContainer.appendChild(titleElement);
+        menuContainer.appendChild(priceElement);
+        menuItemDetails.appendChild(menuContainer);
+
+        const descriptionElement = document.createElement("p");
+        descriptionElement.classList.add("menu-item-description");
+        descriptionElement.textContent = menuItem.description;
+        menuItemDetails.appendChild(descriptionElement);
+
+        menuItemElement.appendChild(menuItemDetails);
+
+        coffeeMenuContainer.appendChild(menuItemElement);
+      });
+    })
+    .catch((error) => {
+      console.log("Error loading coffee menu:", error);
+    });
+}
+
+// Call the function to load the coffee menu
+loadCoffeeMenu();
